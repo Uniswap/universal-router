7b6f40819cfe4651d6eaf9482cfe87115c6ff053
Emily Williams <emag3m@gmail.com>
2022-09-23 14:43:18 -0400

    sending/decoding extra params +1500 gas

--- contracts/Router.sol
+++ contracts/Router.sol
@@ -108,7 +108,7 @@ contract WeirollRouter is V2SwapRouter, V3SwapRouter {
                     abi.decode(inputs, (address, uint256, uint256, bytes));
                 outdata = abi.encode(v3SwapExactOutput(recipient, amountIn, amountOutMin, path));
             } else if (commandType == FLAG_CT_NFT_MARKETPLACE) {
-                (uint256 value, NFTMarketPlaces marketPlace, bytes memory data) = abi.decode(state.buildInputs(indices), (uint256, NFTMarketPlaces, bytes));
+                (uint256 value, bytes memory data) = abi.decode(state.buildInputs(indices), (uint256, bytes));
                 (success, outdata) = Constants.SEAPORT.call{value: value}(data);
             } else if (commandType == FLAG_CT_SWEEP) {
                 (address token, address recipient, uint256 minValue) = abi.decode(inputs, (address, address, uint256));
