b33a7df93097ae6a7e9016cfc87c990ec25380db
Emily Williams <emag3m@gmail.com>
2022-09-26 14:20:45 -0400

    remove weiroll feature to use entire state as input

--- contracts/libraries/CommandBuilder.sol
+++ contracts/libraries/CommandBuilder.sol
@@ -11,8 +11,6 @@ library CommandBuilder {
     function buildInputs(bytes[] memory state, bytes32 indices) internal view returns (bytes memory ret) {
         uint256 count; // Number of bytes in whole ABI encoded message
         uint256 free; // Pointer to first free byte in tail part of message
-        bytes memory stateData; // Optionally encode the current state if the call requires it
-
         uint256 idx;
 
         // Determine the length of the encoded data
@@ -23,17 +21,10 @@ library CommandBuilder {
             }
 
             if (idx & IDX_VARIABLE_LENGTH != 0) {
-                if (idx == IDX_USE_STATE) {
-                    if (stateData.length == 0) {
-                        stateData = abi.encode(state);
-                    }
-                    count += stateData.length;
-                } else {
-                    // Add the size of the value, rounded up to the next word boundary, plus space for pointer and length
-                    uint256 arglen = state[idx & IDX_VALUE_MASK].length;
-                    require(arglen % 32 == 0, 'Dynamic state variables must be a multiple of 32 bytes');
-                    count += arglen + 32;
-                }
+                // Add the size of the value, rounded up to the next word boundary, plus space for pointer and length
+                uint256 arglen = state[idx & IDX_VALUE_MASK].length;
+                require(arglen % 32 == 0, 'Dynamic state variables must be a multiple of 32 bytes');
+                count += arglen + 32;
             } else {
                 require(state[idx & IDX_VALUE_MASK].length == 32, 'Static state variables must be 32 bytes');
                 count += 32;
@@ -48,6 +39,7 @@ library CommandBuilder {
 
         ret = new bytes(count);
         count = 0;
+
         for (uint256 i; i < 32;) {
             idx = uint8(indices[i]);
             if (idx == IDX_END_OF_ARGS) {
@@ -55,22 +47,14 @@ library CommandBuilder {
             }
 
             if (idx & IDX_VARIABLE_LENGTH != 0) {
-                if (idx == IDX_USE_STATE) {
-                    assembly {
-                        mstore(add(add(ret, 32), count), free)
-                    }
-                    memcpy(stateData, 32, ret, free, stateData.length - 32);
-                    free += stateData.length - 32;
-                } else {
-                    uint256 arglen = state[idx & IDX_VALUE_MASK].length;
+                uint256 arglen = state[idx & IDX_VALUE_MASK].length;
 
-                    // Variable length data; put a pointer in the slot and write the data at the end
-                    assembly {
-                        mstore(add(add(ret, 32), count), free)
-                    }
-                    memcpy(state[idx & IDX_VALUE_MASK], 0, ret, free, arglen);
-                    free += arglen;
+                // Variable length data; put a pointer in the slot and write the data at the end
+                assembly {
+                    mstore(add(add(ret, 32), count), free)
                 }
+                memcpy(state[idx & IDX_VALUE_MASK], 0, ret, free, arglen);
+                free += arglen;
             } else {
                 // Fixed length data; write it directly
                 bytes memory statevar = state[idx & IDX_VALUE_MASK];
