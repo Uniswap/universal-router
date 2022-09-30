44140f5561bfe4bb7bafc0ebedc183c3d925184f
Mark Toda <toda.mark@gmail.com>
2022-09-30 13:49:52 -0700

    feat: Proof of concept on some readability ideas
    - move commands to Commands.sol to separate from meta weiroll logic
    - move some hard-to-read bytes manip to CommandLib

---
+++ contracts/base/Commands.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import '../modules/V2SwapRouter.sol';
+import '../modules/V3SwapRouter.sol';
+import '../modules/Payments.sol';
+import '../base/RouterCallbacks.sol';
+import '../Router.sol';
+
+contract Commands is V2SwapRouter, V3SwapRouter, RouterCallbacks {
+    error InvalidCommandType(uint256 commandIndex);
+
+    address immutable PERMIT_POST;
+
+    constructor(
+        address permitPost,
+        address v2Factory,
+        address v3Factory,
+        bytes32 pairInitCodeHash,
+        bytes32 poolInitCodeHash
+    ) V2SwapRouter(v2Factory, pairInitCodeHash) V3SwapRouter(v3Factory, poolInitCodeHash) {
+        PERMIT_POST = permitPost;
+    }
+
+    /// @notice executes the given command with the given inputs
+    /// @param command The command to execute
+    /// @param inputs The inputs to execute the command with
+    /// @return success true on success, false on failure
+    /// @return output The outputs, if any from the command
+    function dispatch(CommandType command, bytes memory inputs, uint256 commandIndex)
+        internal
+        returns (bool success, bytes memory output)
+    {
+        success = true;
+        if (command == CommandType.PERMIT) {
+            // state[state.length] = abi.encode(msg.sender);
+            // (success, output) = permitPost.call(state[0]);
+            // bytes memory inputs = state.build(bytes4(0), indices);
+            // (address some, address parameters, uint256 forPermit) = abi.decode(inputs, (address, address, uint));
+            //
+            // permitPost.permitWithNonce(msg.sender, some, parameters, forPermit);
+        } else if (command == CommandType.TRANSFER) {
+            (address token, address recipient, uint256 value) = abi.decode(inputs, (address, address, uint256));
+            Payments.payERC20(token, recipient, value);
+        } else if (command == CommandType.V2_SWAP_EXACT_IN) {
+            (uint256 amountOutMin, address[] memory path, address recipient) =
+                abi.decode(inputs, (uint256, address[], address));
+            output = abi.encode(v2SwapExactInput(amountOutMin, path, recipient));
+        } else if (command == CommandType.V2_SWAP_EXACT_OUT) {
+            (uint256 amountOut, uint256 amountInMax, address[] memory path, address recipient) =
+                abi.decode(inputs, (uint256, uint256, address[], address));
+            output = abi.encode(v2SwapExactOutput(amountOut, amountInMax, path, recipient));
+        } else if (command == CommandType.V3_SWAP_EXACT_IN) {
+            (address recipient, uint256 amountIn, uint256 amountOutMin, bytes memory path) =
+                abi.decode(inputs, (address, uint256, uint256, bytes));
+            output = abi.encode(v3SwapExactInput(recipient, amountIn, amountOutMin, path));
+        } else if (command == CommandType.V3_SWAP_EXACT_OUT) {
+            (address recipient, uint256 amountIn, uint256 amountOutMin, bytes memory path) =
+                abi.decode(inputs, (address, uint256, uint256, bytes));
+            output = abi.encode(v3SwapExactOutput(recipient, amountIn, amountOutMin, path));
+        } else if (command == CommandType.SEAPORT) {
+            (uint256 value, bytes memory data) = abi.decode(inputs, (uint256, bytes));
+            (success, output) = Constants.SEAPORT.call{value: value}(data);
+        } else if (command == CommandType.NFTX) {
+            (uint256 value, bytes memory data) = abi.decode(inputs, (uint256, bytes));
+            (success, output) = Constants.NFTX_ZAP.call{value: value}(data);
+        } else if (command == CommandType.LOOKS_RARE) {
+            (success, output) = callAndTransferERC721(inputs, Constants.LOOKS_RARE);
+        } else if (command == CommandType.X2Y2) {
+            (success, output) = callAndTransferERC721(inputs, Constants.X2Y2);
+        } else if (command == CommandType.SWEEP) {
+            (address token, address recipient, uint256 minValue) = abi.decode(inputs, (address, address, uint256));
+            Payments.sweepToken(token, recipient, minValue);
+        } else if (command == CommandType.WRAP_ETH) {
+            (address recipient, uint256 amountMin) = abi.decode(inputs, (address, uint256));
+            Payments.wrapETH(recipient, amountMin);
+        } else if (command == CommandType.UNWRAP_WETH) {
+            (address recipient, uint256 amountMin) = abi.decode(inputs, (address, uint256));
+            Payments.unwrapWETH9(recipient, amountMin);
+        } else {
+            revert InvalidCommandType({commandIndex: commandIndex});
+        }
+    }
+
+    function callAndTransferERC721(bytes memory inputs, address protocol)
+        internal
+        returns (bool success, bytes memory output)
+    {
+        (uint256 value, bytes memory data, address recipient, address token, uint256 id) =
+            abi.decode(inputs, (uint256, bytes, address, address, uint256));
+        (success, output) = protocol.call{value: value}(data);
+        if (success) ERC721(token).safeTransferFrom(address(this), recipient, id);
+    }
+}
---
+++ contracts/libraries/CommandLib.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import './BytesLib.sol';
+import '../Router.sol';
+
+library CommandLib {
+    using BytesLib for bytes;
+
+    /// @notice mask for parsing command type
+    uint8 constant FLAG_COMMAND_TYPE_MASK = 0x0f;
+    /// @notice offset of the command indices in a given command
+    uint8 constant COMMAND_INDICES_OFFSET = 8;
+    /// @notice The offset of the actual data in bytes
+    uint8 constant PARAMS_LENGTH_OFFSET = 32;
+    /// @notice the length of each command in bytes
+    uint8 constant COMMAND_LENGTH_BYTES = 8;
+
+    /// @notice Returns the number of commands in the list
+    /// @param commands The encoded commands list
+    /// @return The number of commands in the list
+    function numCommands(bytes memory commands) internal pure returns (uint256) {
+        return commands.length / COMMAND_LENGTH_BYTES;
+    }
+
+    /// @notice Returns true iff the commands bytes contains another command
+    /// @param commands The encoded commands
+    /// @return True if commands bytes contains another command, else false
+    function hasNextCommand(bytes memory commands) internal pure returns (bool) {
+        return commands.length >= COMMAND_LENGTH_BYTES;
+    }
+
+    /// @notice Fetch the data for the first command in the command bytes
+    function decodeCommand(bytes memory commands, uint256 index)
+        internal
+        pure
+        returns (uint8 flags, CommandType commandType, bytes8 indices)
+    {
+        bytes8 command;
+        assembly {
+            // loads the command at index
+            command := mload(add(add(commands, PARAMS_LENGTH_OFFSET), mul(COMMAND_LENGTH_BYTES, index)))
+        }
+
+        flags = uint8(bytes1(command));
+        commandType = CommandType(flags & FLAG_COMMAND_TYPE_MASK);
+        indices = bytes8(uint64(command) << COMMAND_INDICES_OFFSET);
+    }
+
+    /// @notice Skips a command from the buffer and returns the remainder
+    /// @param commands The command bytes
+    /// @return The remaining commands after skipping the first one
+    function skipCommand(bytes memory commands) internal pure returns (bytes memory) {
+        return commands.slice(COMMAND_LENGTH_BYTES, commands.length - COMMAND_LENGTH_BYTES);
+    }
+}
