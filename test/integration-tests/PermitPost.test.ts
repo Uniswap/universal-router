560e0075afebc4a474290d3bbde39d8b7d6cd340
Alice Henshaw <henshawalice@gmail.com>
2022-10-06 16:06:01 -0400

    Permit post tests passing

--- test/integration-tests/PermitPost.test.ts
+++ test/integration-tests/PermitPost.test.ts
@@ -18,17 +18,15 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import hre from 'hardhat'
 import { expect } from 'chai'
 import { expandTo18DecimalsBN } from './shared/helpers'
+import snapshotGasCost from '@uniswap/snapshot-gas-cost'
 const { ethers } = hre
 import { BigNumber } from 'ethers'
-import fs from 'fs'
-import { defaultAbiCoder as abiCoder, keccak256 } from 'ethers/lib/utils'
 
-describe.only('PermitPost', () => {
+describe('PermitPost', () => {
   let alice: SignerWithAddress
   let bob: SignerWithAddress
   let router: Router
   let permitPost: Contract
-  let value: BigNumber
   let planner: RouterPlanner
   let wethContract: Contract
   let routerFactory: ContractFactory
@@ -36,13 +34,6 @@ describe.only('PermitPost', () => {
 
   const permitPostInterface = new ethers.utils.Interface(PERMIT_POST_COMPILE.abi)
   const permitPostBytecode = PERMIT_POST_COMPILE.bytecode
-
-  // constants used for signatures in permit post
-  let TOKEN_DETAILS_TYPEHASH: string
-  let PERMIT_TYPEHASH: string
-  const VERSION_HASH = keccak256(ethers.utils.toUtf8Bytes("1"))
-  const NAME_HASH = keccak256(ethers.utils.toUtf8Bytes("PermitPost"))
-  const TYPE_HASH = keccak256(ethers.utils.toUtf8Bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"))
 
   type TokenDetails = {
     tokenType: number
@@ -64,6 +55,30 @@ describe.only('PermitPost', () => {
     s: string
   }
 
+  const eip712Domain = {
+    name: 'PermitPost',
+    version: '1',
+    chainId: hre.network.config.chainId,
+    verifyingContract: ethers.constants.AddressZero,
+  };
+
+  const eip712Types = {
+    TokenDetails: [
+      { name: 'tokenType', type: 'uint8' },
+      { name: 'token', type: 'address' },
+      { name: 'maxAmount', type: 'uint256' },
+      { name: 'id', type: 'uint256' }
+    ],
+    Permit: [
+      { name: 'sigType', type: 'uint8' },
+      { name: 'tokens', type: 'TokenDetails[]' },
+      { name: 'spender', type: 'address' },
+      { name: 'deadline', type: 'uint256' },
+      { name: 'witness', type: 'bytes32' },
+      { name: 'nonce', type: 'uint256' }
+    ]
+  };
+
   function constructData(
     permit: Permit,
     to: string[],
@@ -82,46 +97,22 @@ describe.only('PermitPost', () => {
   }
 
   async function signPermit(permit: Permit, signatureType: number, nonce: number, signer: SignerWithAddress): Promise<Signature> {
-    // first hash each token details
-    const hashedTokenDetails = permit.tokens.map((details) => {
-      const encodedTokenDetails = abiCoder.encode(
-        ['bytes32', 'uint8', 'address', 'uint256', 'uint256'],
-        [TOKEN_DETAILS_TYPEHASH, details.tokenType, details.token, details.maxAmount, details.id]
-      )
-      return keccak256(encodedTokenDetails)
-    })
+    const eip712Values = {
+      sigType: signatureType,
+      tokens: permit.tokens,
+      spender: permit.spender,
+      deadline: permit.deadline,
+      witness: permit.witness,
+      nonce: nonce
+    }
 
-    // encodePacked and hash the token hashes
-    const tokensHash = keccak256(abiCoder.encode(Array(hashedTokenDetails.length).fill('bytes32'), hashedTokenDetails))
-
-    // then encode and hash the permit details
-    const encodedPermit = abiCoder.encode(
-      ['bytes32', 'uint8', 'bytes32', 'address', 'uint256', 'bytes32', 'uint256'],
-      [PERMIT_TYPEHASH, signatureType, tokensHash, permit.spender, permit.deadline, permit.witness, nonce]
-    )
-    const hashedPermit = keccak256(encodedPermit)
-
-    // add domain separator and perform a typed data hash (using EIP191 signed message)
-    const DOMAIN_SEPARATOR = keccak256(abiCoder.encode(
-      ['bytes32', 'bytes32', 'bytes32', 'uint256', 'address'],
-      [TYPE_HASH, NAME_HASH, VERSION_HASH, hre.network.config.chainId, permitPost.address]
-    ))
-    const msgHash = ethers.utils.hashMessage(abiCoder.encode(['bytes32', 'bytes32'], [DOMAIN_SEPARATOR, hashedPermit]))
-
-    // then sign the permit
-    const signature = await signer.signMessage(ethers.utils.arrayify(msgHash))
+    const signature = await signer._signTypedData(eip712Domain, eip712Types, eip712Values)
 
     return {
       r: '0x' + signature.slice(2, 66),
       s: '0x' + signature.slice(66, 130),
       v: Number('0x' + signature.slice(130)),
     }
-  }
-
-  async function fetchEIP712Constants() {
-    permitPost = await permitPostFactory.deploy()
-    PERMIT_TYPEHASH = await permitPost._PERMIT_TYPEHASH()
-    TOKEN_DETAILS_TYPEHASH = await permitPost._TOKEN_DETAILS_TYPEHASH()
   }
 
   before(async () => {
@@ -133,9 +124,6 @@ describe.only('PermitPost', () => {
     routerFactory = await ethers.getContractFactory('Router')
 
     wethContract = new ethers.Contract(WETH.address, TOKEN_ABI, alice)
-
-    // gather the constants used in signature production
-    await fetchEIP712Constants()
   })
 
   beforeEach(async () => {
@@ -152,8 +140,10 @@ describe.only('PermitPost', () => {
         V2_INIT_CODE_HASH_MAINNET,
         V3_INIT_CODE_HASH_MAINNET
       )
-    ).connect(alice) as Router
+    ).connect(bob) as Router
     planner = new RouterPlanner()
+
+    eip712Domain.verifyingContract = permitPost.address
 
     // Given we must use Bob to test this contract, Alice gives Bob 1 WETH
     await wethContract.connect(alice).transfer(bob.address, expandTo18DecimalsBN(10))
@@ -161,14 +151,13 @@ describe.only('PermitPost', () => {
     await wethContract.connect(bob).approve(permitPost.address, MAX_UINT)
   })
 
-  it('Fetch ERC20 via permit post ', async () => {
+  it('Fetch ERC20 via permit post', async () => {
     // We construct Bob's permit
-    const id = BigNumber.from(0)
     const tokenDetails: TokenDetails = {
       tokenType: 0, // ERC20
       token: WETH.address,
       maxAmount: expandTo18DecimalsBN(2),
-      id,
+      id: BigNumber.from(0)
     }
 
     const permit: Permit = {
@@ -179,7 +168,6 @@ describe.only('PermitPost', () => {
     }
 
     const signatureType: number = 1 // sequential
-
     const nonce: number = 0 // currently no nonces have been used
 
     // Now Bob signs this payload
@@ -195,12 +183,44 @@ describe.only('PermitPost', () => {
 
     planner.add(PermitCommand(calldata))
     const { commands, state } = planner.plan()
-    await router.execute(DEADLINE, commands, state, { value: value })
+    await router.execute(DEADLINE, commands, state)
 
     const bobBalanceAfter = await wethContract.balanceOf(bob.address)
     const routerBalanceAfter = await wethContract.balanceOf(router.address)
 
     expect(bobBalanceBefore).to.be.eq(bobBalanceAfter.add(amountToTransfer))
     expect(routerBalanceBefore).to.be.eq(routerBalanceAfter.sub(amountToTransfer))
+  })
+
+  it('gas: transfer ERC20 into router', async () => {
+    // We construct Bob's permit
+    const tokenDetails: TokenDetails = {
+      tokenType: 0, // ERC20
+      token: WETH.address,
+      maxAmount: expandTo18DecimalsBN(2),
+      id: BigNumber.from(0)
+    }
+
+    const permit: Permit = {
+      tokens: [tokenDetails],
+      spender: router.address, // the router is the one who will claim the WETH
+      deadline: BigNumber.from(MAX_UINT),
+      witness: EMPTY_BYTES_32,
+    }
+
+    const signatureType: number = 1 // sequential
+    const nonce: number = 0 // currently no nonces have been used
+
+    // Now Bob signs this payload
+    const signature = await signPermit(permit, signatureType, nonce, bob)
+
+    // Construct the permit post transferFrom calldata, without the function selector first parameter
+    // The resulting calldata is what we pass into permit post
+    const amountToTransfer = expandTo18DecimalsBN(1)
+    const calldata = constructData(permit, [router.address], [amountToTransfer], signature)
+
+    planner.add(PermitCommand(calldata))
+    const { commands, state } = planner.plan()
+    await snapshotGasCost(router.execute(DEADLINE, commands, state))
   })
 })
