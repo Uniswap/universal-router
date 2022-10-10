0ee21c61f3245407b7ce0a64f17a5d258b2f4e27
Alice Henshaw <henshawalice@gmail.com>
2022-10-10 15:45:44 -0400

    setup for user scenario tests

--- test/integration-tests/PermitPost.integrations.test.ts
+++ test/integration-tests/PermitPost.integrations.test.ts
@@ -1,7 +1,7 @@
 import { RouterPlanner, PermitCommand, V2ExactInputCommand } from '@uniswap/narwhal-sdk'
 import type { Contract, ContractFactory } from '@ethersproject/contracts'
 import { Router } from '../../typechain'
-import { DAI, resetFork, WETH } from './shared/mainnetForkHelpers'
+import { DAI, executeSwap, resetFork, SWAP_ROUTER_V2, USDC, WETH } from './shared/mainnetForkHelpers'
 import PERMIT_POST_COMPILE from '../../lib/permitpost/out/PermitPost.sol/PermitPost.json'
 import {
   EMPTY_BYTES_32,
@@ -14,6 +14,7 @@ import {
   V3_INIT_CODE_HASH_MAINNET,
 } from './shared/constants'
 import { abi as TOKEN_ABI } from '../../artifacts/@openzeppelin/contracts/token/ERC20/IERC20.sol/IERC20.json'
+import { Route as V3RouteSDK, FeeAmount } from '@uniswap/v3-sdk'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import hre from 'hardhat'
 import { expandTo18DecimalsBN } from './shared/helpers'
@@ -28,13 +29,17 @@ import {
   signPermit,
   TokenDetails,
 } from './shared/protocolHelpers/permitPost'
+import { SwapRouter, MixedRouteSDK, Trade } from '@uniswap/router-sdk'
+import { CurrencyAmount, Ether, Percent, Token, TradeType } from '@uniswap/sdk-core'
+import { pool_DAI_USDC, pool_DAI_WETH, pool_USDC_WETH } from './shared/swapRouter02Helpers'
 
-describe('PermitPost Integrations', () => {
+describe.only('PermitPost Integrations', () => {
   let alice: SignerWithAddress
   let bob: SignerWithAddress
   let router: Router
   let permitPost: Contract
   let planner: RouterPlanner
+  let usdcContract: Contract
   let wethContract: Contract
   let routerFactory: ContractFactory
   let permitPostFactory: ContractFactory
@@ -46,6 +51,10 @@ describe('PermitPost Integrations', () => {
 
   const chainId: number = hre.network.config.chainId ? hre.network.config.chainId : 1
 
+  function getUSDCAmountIn(amount: number) : number {
+    return amount * (10 ** 6)
+  }
+
   before(async () => {
     await resetFork()
     alice = await ethers.getSigner(ALICE_ADDRESS)
@@ -54,6 +63,7 @@ describe('PermitPost Integrations', () => {
     permitPostFactory = new ethers.ContractFactory(PERMIT_POST_INTERFACE, permitPostBytecode, alice)
     routerFactory = await ethers.getContractFactory('Router')
 
+    usdcContract = new ethers.Contract(USDC.address, TOKEN_ABI, alice)
     wethContract = new ethers.Contract(WETH.address, TOKEN_ABI, alice)
   })
 
@@ -74,7 +84,8 @@ describe('PermitPost Integrations', () => {
     ).connect(bob) as Router
     planner = new RouterPlanner()
 
-    // Given we must use Bob to test this contract, Alice gives Bob 1 WETH
+    // Given we must use Bob to test this contract, Alice gives Bob 100,000 USDC and 10 WETH
+    await usdcContract.connect(alice).transfer(bob.address, getUSDCAmountIn(100000))
     await wethContract.connect(alice).transfer(bob.address, expandTo18DecimalsBN(10))
     // Bob approves the permit post contract to transfer funds
     await wethContract.connect(bob).approve(permitPost.address, MAX_UINT)
@@ -116,5 +127,44 @@ describe('PermitPost Integrations', () => {
     planner.add(V2ExactInputCommand(expandTo18DecimalsBN(1000), [WETH.address, DAI.address], alice.address))
     const { commands, state } = planner.plan()
     await snapshotGasCost(router.execute(DEADLINE, commands, state))
+  })
+
+  const slippageTolerance = new Percent(10, 100)
+  const deadline = 2000000000
+  const simpleSwapAmountIn = CurrencyAmount.fromRawAmount(USDC, getUSDCAmountIn(1000))
+  let v3ExactInMultihop: Trade<Token, Token, TradeType.EXACT_INPUT>
+
+  describe('First Time User.', () => {
+    describe('Simple Swap.', () => {
+      describe('SwapRouter02.', () => {
+        it('Max Approve SwapRouter02', async () => {
+          await snapshotGasCost(usdcContract.approve(SWAP_ROUTER_V2, MAX_UINT))
+        })
+
+        it('Swap SwapRouter02', async () => {
+          // USDC -> WETH -> DAI
+          v3ExactInMultihop = await Trade.fromRoute(
+            new V3RouteSDK([pool_USDC_WETH, pool_DAI_WETH], USDC, DAI),
+            simpleSwapAmountIn,
+            TradeType.EXACT_INPUT
+          )
+          const { calldata } = SwapRouter.swapCallParameters(v3ExactInMultihop, {
+            slippageTolerance,
+            recipient: bob.address,
+            deadlineOrPreviousBlockhash: 2000000000,
+          })
+  
+          await snapshotGasCost(executeSwap({ value: '0', calldata }, USDC, DAI, bob))
+        })
+      })
+
+      describe('PP Sign-Per-Swap.', () => {
+        it('Max Approve Permit2', async () => {
+          await snapshotGasCost(usdcContract.approve(permitPost.address, MAX_UINT))
+        })
+
+        it('Swap Narwhal')
+      })
+    })
   })
 })
