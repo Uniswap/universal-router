8323eca979bfa5251a0bbc45a8b14645156bd1f8
Allen Lin <allenlin1992@hotmail.com>
2022-10-11 13:09:52 -0400

    continue

--- test/integration-tests/Foundation.test.ts
+++ test/integration-tests/Foundation.test.ts
@@ -1,6 +1,6 @@
 import {RouterPlanner, FoundationCommand} from '@uniswap/narwhal-sdk'
 import FOUNDATION_ABI from './shared/abis/Foundation.json'
-import { Router } from '../../typechain'
+import { Router, ERC721 } from '../../typechain'
 import { resetFork } from './shared/mainnetForkHelpers'
 import {
     ALICE_ADDRESS,
@@ -10,13 +10,17 @@ import {
     V2_INIT_CODE_HASH_MAINNET,
     V3_INIT_CODE_HASH_MAINNET,
 } from './shared/constants'
-// import snapshotGasCost from '@uniswap/snapshot-gas-cost'
+import snapshotGasCost from '@uniswap/snapshot-gas-cost'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import hre from 'hardhat'
 import { BigNumber } from 'ethers'
+import {abi as ERC721_ABI} from "../../artifacts/solmate/src/tokens/ERC721.sol/ERC721.json";
+import {expect} from "chai";
 const { ethers } = hre
 
 const FOUNDATION_INTERFACE = new ethers.utils.Interface(FOUNDATION_ABI)
+const MENTAL_WORLDS_ADDRESS = '0xEf96021Af16BD04918b0d87cE045d7984ad6c38c';
+const REFERRER = '0x459e213D8B5E79d706aB22b945e3aF983d51BC4C';
 
 describe('Foundation', () => {
     let alice: SignerWithAddress
@@ -28,7 +32,11 @@ describe('Foundation', () => {
         alice = await ethers.getSigner(ALICE_ADDRESS)
     })
 
-    describe('ERC-721 purchase', () => {
+    // In this test we will buy token id 32 of mental worlds NFT (0xEf96021Af16BD04918b0d87cE045d7984ad6c38c),
+    // which costs 0.01 ETH
+    describe('Buy a mental worlds NFT from Foundation', () => {
+
+        let mentalWorlds: ERC721
 
         beforeEach(async () => {
             await resetFork(15725945)
@@ -46,33 +54,45 @@ describe('Foundation', () => {
                     V3_INIT_CODE_HASH_MAINNET
                 )
             ).connect(alice) as Router
+
+            mentalWorlds = new ethers.Contract(MENTAL_WORLDS_ADDRESS, ERC721_ABI) as ERC721
         })
 
-        it.only('purchases 1 ERC-721 on Foundation', async () => {
+        it('purchases token id 32 of mental worlds', async () => {
 
             const value = BigNumber.from('10000000000000000');
             const calldata = FOUNDATION_INTERFACE.encodeFunctionData('buyV2', [
-                '0xEf96021Af16BD04918b0d87cE045d7984ad6c38c',
+                MENTAL_WORLDS_ADDRESS,
                 32,
                 value,
-                '0x459e213D8B5E79d706aB22b945e3aF983d51BC4C'
+                REFERRER
             ])
-            planner.add(FoundationCommand(value, calldata, ALICE_ADDRESS, '0xEf96021Af16BD04918b0d87cE045d7984ad6c38c', 32))
+            planner.add(FoundationCommand(value, calldata, ALICE_ADDRESS, MENTAL_WORLDS_ADDRESS, 32))
             const { commands, state } = planner.plan()
 
+            const aliceBalance = await ethers.provider.getBalance(alice.address);
+            const referrerBalance = await ethers.provider.getBalance(REFERRER);
             const receipt = await (await router.execute(DEADLINE, commands, state, { value: value })).wait()
-            console.log(receipt)
-            // const erc721TransferEvent = parseEvents(ERC721_INTERFACE, receipt)[1]?.args!
 
-            // const newOwner = await ENS_721.connect(alice).ownerOf(erc721Order.token_id)
-            // await expect(newOwner.toLowerCase()).to.eq(ALICE_ADDRESS)
-            // await expect(erc721TransferEvent.from).to.be.eq(router.address)
-            // await expect(erc721TransferEvent.to.toLowerCase()).to.be.eq(ALICE_ADDRESS)
-            // await expect(erc721TransferEvent.id).to.be.eq(erc721Order.token_id)
+            // Expect that alice has the NFT
+            await expect((await mentalWorlds.connect(alice).ownerOf(32)).toLowerCase()).to.eq(ALICE_ADDRESS)
+            // Expect that alice's account has 0.01 (plus gas) less ETH in it
+            await expect(aliceBalance.sub(await ethers.provider.getBalance(alice.address))).to.eq(value.add(receipt.gasUsed.mul(receipt.effectiveGasPrice)));
+            // Expect that referrer's account has 0.0001 more ETH in it (referrers receive 1% of NFT value)
+            await expect((await ethers.provider.getBalance(REFERRER)).sub(referrerBalance)).to.eq(value.div(100));
         })
 
-        // it('gas purchases 1 ERC-721 on Foundation', async () => {
-        //     await snapshotGasCost(router.execute(DEADLINE, commands, state, { value: erc721Order.price }))
-        // })
+        it('gas token id 32 of mental worlds', async () => {
+            const value = BigNumber.from('10000000000000000');
+            const calldata = FOUNDATION_INTERFACE.encodeFunctionData('buyV2', [
+                MENTAL_WORLDS_ADDRESS,
+                32,
+                value,
+                REFERRER
+            ])
+            planner.add(FoundationCommand(value, calldata, ALICE_ADDRESS, MENTAL_WORLDS_ADDRESS, 32))
+            const { commands, state } = planner.plan()
+            await snapshotGasCost(router.execute(DEADLINE, commands, state, { value: value }))
+        })
     })
 })
