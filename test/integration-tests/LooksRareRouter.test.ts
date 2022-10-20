bb334e2a2cb45e1787dd3349582e522a873fdd2e
Alice Henshaw <henshawalice@gmail.com>
2022-10-20 10:52:48 -0300

    Make common looks rare helpers

--- test/integration-tests/LooksRareRouter.test.ts
+++ test/integration-tests/LooksRareRouter.test.ts
@@ -1,6 +1,5 @@
 import { CommandType, RoutePlanner } from './shared/planner'
 import { Router, ERC721, ERC1155 } from '../../typechain'
-import LOOKS_RARE_ABI from './shared/abis/LooksRare.json'
 import { resetFork, COVEN_721, TWERKY_1155 } from './shared/mainnetForkHelpers'
 import {
   ALICE_ADDRESS,
@@ -17,72 +16,15 @@ import hre from 'hardhat'
 import { expect } from 'chai'
 const { ethers } = hre
 import { BigNumber } from 'ethers'
-import fs from 'fs'
-
-const looksRareOrders = JSON.parse(
-  fs.readFileSync('test/integration-tests/shared/orders/LooksRare.json', { encoding: 'utf8' })
-)
-const looksRareInterface = new ethers.utils.Interface(LOOKS_RARE_ABI)
-
-const ERC_721_ORDER_INDEX = 0
-const ERC_1155_ORDER_INDEX = 2
-
-type APIOrder = Omit<MakerOrder, 'collection' | 'currency'> & {
-  collectionAddress: string
-  currencyAddress: string
-}
-
-type MakerOrder = {
-  collection: string
-  tokenId: BigNumber
-  isOrderAsk: true
-  signer: string
-  strategy: string
-  currency: string
-  amount: BigNumber
-  price: BigNumber
-  minPercentageToAsk: BigNumber
-  nonce: BigNumber
-  startTime: BigNumber
-  endTime: BigNumber
-  v: BigNumber
-  r: string
-  s: string
-  params: string
-}
-
-type TakerOrder = {
-  minPercentageToAsk: BigNumber
-  price: BigNumber
-  taker: string
-  tokenId: BigNumber
-  isOrderAsk: boolean
-  params: string
-}
-
-function createLooksRareOrders(
-  apiOrder: APIOrder,
-  taker: string
-): { makerOrder: MakerOrder; takerOrder: TakerOrder; value: BigNumber } {
-  const collection = apiOrder.collectionAddress
-  const currency = apiOrder.currencyAddress
-  if (apiOrder.params == '') apiOrder.params = '0x'
-
-  const makerOrder = { ...apiOrder, collection, currency }
-
-  const takerOrder = {
-    minPercentageToAsk: apiOrder.minPercentageToAsk,
-    price: apiOrder.price,
-    taker,
-    tokenId: apiOrder.tokenId,
-    isOrderAsk: false,
-    params: apiOrder.params,
-  }
-
-  const value = BigNumber.from(apiOrder.price)
-
-  return { makerOrder, takerOrder, value }
-}
+import {
+  createLooksRareOrders,
+  looksRareInterface,
+  looksRareOrders,
+  LOOKS_RARE_1155_ORDER,
+  LOOKS_RARE_721_ORDER,
+  MakerOrder,
+  TakerOrder,
+} from './shared/protocolHelpers/looksrare'
 
 describe('LooksRare', () => {
   let alice: SignerWithAddress
@@ -122,7 +64,10 @@ describe('LooksRare', () => {
     let tokenId: BigNumber
 
     beforeEach(async () => {
-      ;({ makerOrder, takerOrder, value } = createLooksRareOrders(looksRareOrders[ERC_721_ORDER_INDEX], router.address))
+      ;({ makerOrder, takerOrder, value } = createLooksRareOrders(
+        looksRareOrders[LOOKS_RARE_721_ORDER],
+        router.address
+      ))
       tokenId = makerOrder.tokenId
     })
 
@@ -151,7 +96,7 @@ describe('LooksRare', () => {
 
     beforeEach(async () => {
       ;({ makerOrder, takerOrder, value } = createLooksRareOrders(
-        looksRareOrders[ERC_1155_ORDER_INDEX],
+        looksRareOrders[LOOKS_RARE_1155_ORDER],
         router.address
       ))
       tokenId = makerOrder.tokenId
--- test/integration-tests/gas-tests/LooksRareRouter.gas.test.ts
+++ test/integration-tests/gas-tests/LooksRareRouter.gas.test.ts
@@ -1,6 +1,5 @@
 import { CommandType, RoutePlanner } from './../shared/planner'
 import { Router } from '../../../typechain'
-import LOOKS_RARE_ABI from './../shared/abis/LooksRare.json'
 import { resetFork } from './../shared/mainnetForkHelpers'
 import {
   ALICE_ADDRESS,
@@ -17,72 +16,15 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import hre from 'hardhat'
 const { ethers } = hre
 import { BigNumber } from 'ethers'
-import fs from 'fs'
-
-const looksRareOrders = JSON.parse(
-  fs.readFileSync('test/integration-tests/shared/orders/LooksRare.json', { encoding: 'utf8' })
-)
-const looksRareInterface = new ethers.utils.Interface(LOOKS_RARE_ABI)
-
-const ERC_721_ORDER_INDEX = 0
-const ERC_1155_ORDER_INDEX = 2
-
-type APIOrder = Omit<MakerOrder, 'collection' | 'currency'> & {
-  collectionAddress: string
-  currencyAddress: string
-}
-
-type MakerOrder = {
-  collection: string
-  tokenId: BigNumber
-  isOrderAsk: true
-  signer: string
-  strategy: string
-  currency: string
-  amount: BigNumber
-  price: BigNumber
-  minPercentageToAsk: BigNumber
-  nonce: BigNumber
-  startTime: BigNumber
-  endTime: BigNumber
-  v: BigNumber
-  r: string
-  s: string
-  params: string
-}
-
-type TakerOrder = {
-  minPercentageToAsk: BigNumber
-  price: BigNumber
-  taker: string
-  tokenId: BigNumber
-  isOrderAsk: boolean
-  params: string
-}
-
-function createLooksRareOrders(
-  apiOrder: APIOrder,
-  taker: string
-): { makerOrder: MakerOrder; takerOrder: TakerOrder; value: BigNumber } {
-  const collection = apiOrder.collectionAddress
-  const currency = apiOrder.currencyAddress
-  if (apiOrder.params == '') apiOrder.params = '0x'
-
-  const makerOrder = { ...apiOrder, collection, currency }
-
-  const takerOrder = {
-    minPercentageToAsk: apiOrder.minPercentageToAsk,
-    price: apiOrder.price,
-    taker,
-    tokenId: apiOrder.tokenId,
-    isOrderAsk: false,
-    params: apiOrder.params,
-  }
-
-  const value = BigNumber.from(apiOrder.price)
-
-  return { makerOrder, takerOrder, value }
-}
+import {
+  createLooksRareOrders,
+  looksRareOrders,
+  MakerOrder,
+  TakerOrder,
+  looksRareInterface,
+  LOOKS_RARE_1155_ORDER,
+  LOOKS_RARE_721_ORDER,
+} from '../shared/protocolHelpers/looksrare'
 
 describe('LooksRare Gas Tests', () => {
   let alice: SignerWithAddress
@@ -118,7 +60,10 @@ describe('LooksRare Gas Tests', () => {
     let tokenId: BigNumber
 
     beforeEach(async () => {
-      ;({ makerOrder, takerOrder, value } = createLooksRareOrders(looksRareOrders[ERC_721_ORDER_INDEX], router.address))
+      ;({ makerOrder, takerOrder, value } = createLooksRareOrders(
+        looksRareOrders[LOOKS_RARE_721_ORDER],
+        router.address
+      ))
       tokenId = makerOrder.tokenId
     })
 
@@ -152,7 +97,7 @@ describe('LooksRare Gas Tests', () => {
 
     beforeEach(async () => {
       ;({ makerOrder, takerOrder, value } = createLooksRareOrders(
-        looksRareOrders[ERC_1155_ORDER_INDEX],
+        looksRareOrders[LOOKS_RARE_1155_ORDER],
         router.address
       ))
       tokenId = makerOrder.tokenId
