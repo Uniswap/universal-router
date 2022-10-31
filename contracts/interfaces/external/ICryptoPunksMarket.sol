9ae2cbd1f46432016af6a02d062a31795e7f1ae2
Allen Lin <allenlin1992@hotmail.com>
2022-10-31 12:56:29 -0400

    fix comment

--- contracts/interfaces/external/ICryptoPunksMarket.sol
+++ contracts/interfaces/external/ICryptoPunksMarket.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.4;
 
-/// @title Interface for WETH9
+/// @title Interface for CryptoPunksMarket
 interface ICryptoPunksMarket {
     function buyPunk(uint256 punkIndex) external payable;
 
