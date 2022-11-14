880f497d4bc9d76c7b64c4c7956d43b4a2fff1d3
marktoda <toda.mark@gmail.com>
2022-11-14 13:44:08 -0800

    fix: sweep erc1155 to actually sweep (#147)
    * fix: sweep erc1155 to actually sweep
    
    Previously it was just transferring tokens, now it actually sweeps! Also
    using forge tests for this functionality since we didnt previously have
    any non-fork tests and seemed like these simple ones were ok to go there
    :)
    
    * fix: merge conflicts oops
    
    * fix: tests
    
    Co-authored-by: Alice Henshaw <henshawalice@gmail.com>

---
+++ test/foundry-tests/mock/MockERC20.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.17;
+
+import {ERC20} from 'solmate/tokens/ERC20.sol';
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20('TEST', 'test', 18) {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
