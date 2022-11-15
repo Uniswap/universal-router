7bbb92fb3e50bf5cfb0468e0b1e738628a9f9e08
Emily Williams <emag3m@gmail.com>
2022-11-15 16:12:50 -0500

    fill in missing command number

--- contracts/libraries/Commands.sol
+++ contracts/libraries/Commands.sol
@@ -18,12 +18,12 @@ library Commands {
     uint256 constant TRANSFER = 0x05;
     uint256 constant PAY_PORTION = 0x06;
 
-    uint256 constant V2_SWAP_EXACT_IN = 0x08;
-    uint256 constant V2_SWAP_EXACT_OUT = 0x09;
-    uint256 constant PERMIT2_PERMIT = 0x0a;
-    uint256 constant WRAP_ETH = 0x0b;
-    uint256 constant UNWRAP_WETH = 0x0c;
-    uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0d;
+    uint256 constant V2_SWAP_EXACT_IN = 0x07;
+    uint256 constant V2_SWAP_EXACT_OUT = 0x08;
+    uint256 constant PERMIT2_PERMIT = 0x09;
+    uint256 constant WRAP_ETH = 0x0a;
+    uint256 constant UNWRAP_WETH = 0x0b;
+    uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0c;
 
     // NFT-related command types
     uint256 constant SEAPORT = 0x10;
