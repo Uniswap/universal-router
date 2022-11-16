123a30043b11444f29b121b0ecb27ae1ab6ad0f4
Alice Henshaw <henshawalice@gmail.com>
2022-11-16 17:00:46 -0300

    more natspec

--- contracts/libraries/Recipient.sol
+++ contracts/libraries/Recipient.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.17;
 
 import {Constants} from '../libraries/Constants.sol';
 
+/// @title Recipient Library
+/// @notice Calculates the recipient address for a command
 library Recipient {
+
+    /// @notice Calculates the recipient address for a command
+    /// @param recipient The recipient or recipient-flag for the command
+    /// @return output The resultant recipient for the command
     function map(address recipient) internal view returns (address) {
         if (recipient == Constants.MSG_SENDER) {
             return msg.sender;
