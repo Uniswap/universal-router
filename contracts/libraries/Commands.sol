a917cd0ebd6f50a39a4a7f0888a4ac19d43334e6
Emily Williams <emag3m@gmail.com>
2022-11-21 08:35:06 -0500

    try without nested if (about to make commit with nesting)

--- contracts/libraries/Commands.sol
+++ contracts/libraries/Commands.sol
@@ -12,7 +12,7 @@ library Commands {
 
     // Command Types. Maximum supported command at this moment is 0x1F.
 
-    // Command Types where value<0x08, executed in the first nested-if block
+    // Command Types where value<0x07, executed in the first nested-if block
     uint256 constant V3_SWAP_EXACT_IN = 0x00;
     uint256 constant V3_SWAP_EXACT_OUT = 0x01;
     uint256 constant PERMIT2_TRANSFER_FROM = 0x02;
@@ -21,29 +21,29 @@ library Commands {
     uint256 constant TRANSFER = 0x05;
     uint256 constant PAY_PORTION = 0x06;
 
-    // Command Types where 0x08<=value<=0x0f, executed in the second nested-if block
-    uint256 constant V2_SWAP_EXACT_IN = 0x08;
-    uint256 constant V2_SWAP_EXACT_OUT = 0x09;
-    uint256 constant PERMIT2_PERMIT = 0x0a;
-    uint256 constant WRAP_ETH = 0x0b;
-    uint256 constant UNWRAP_WETH = 0x0c;
-    uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0d;
+    // Command Types where 0x07<=value<=0x0c, executed in the second nested-if block
+    uint256 constant V2_SWAP_EXACT_IN = 0x07;
+    uint256 constant V2_SWAP_EXACT_OUT = 0x08;
+    uint256 constant PERMIT2_PERMIT = 0x09;
+    uint256 constant WRAP_ETH = 0x0a;
+    uint256 constant UNWRAP_WETH = 0x0b;
+    uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0c;
 
-    // Command Types where 0x10<=value<0x18, executed in the third nested-if block
-    uint256 constant SEAPORT = 0x10;
-    uint256 constant LOOKS_RARE_721 = 0x11;
-    uint256 constant NFTX = 0x12;
-    uint256 constant CRYPTOPUNKS = 0x13;
-    uint256 constant LOOKS_RARE_1155 = 0x14;
-    uint256 constant OWNER_CHECK_721 = 0x15;
-    uint256 constant OWNER_CHECK_1155 = 0x16;
-    uint256 constant SWEEP_ERC721 = 0x17;
+    // Command Types where 0x0d<=value<0x14, executed in the third nested-if block
+    uint256 constant SEAPORT = 0x0d;
+    uint256 constant LOOKS_RARE_721 = 0x0e;
+    uint256 constant NFTX = 0x0f;
+    uint256 constant CRYPTOPUNKS = 0x10;
+    uint256 constant LOOKS_RARE_1155 = 0x11;
+    uint256 constant OWNER_CHECK_721 = 0x12;
+    uint256 constant OWNER_CHECK_1155 = 0x13;
+    uint256 constant SWEEP_ERC721 = 0x14;
 
-    // Command Types where 0x18<=value<=0x1f, executed in the final nested-if block
-    uint256 constant X2Y2_721 = 0x18;
-    uint256 constant SUDOSWAP = 0x19;
-    uint256 constant NFT20 = 0x1a;
-    uint256 constant X2Y2_1155 = 0x1b;
-    uint256 constant FOUNDATION = 0x1c;
-    uint256 constant SWEEP_ERC1155 = 0x1d;
+    // Command Types where 0x15<=value<=0x1a, executed in the final nested-if block
+    uint256 constant X2Y2_721 = 0x15;
+    uint256 constant SUDOSWAP = 0x16;
+    uint256 constant NFT20 = 0x17;
+    uint256 constant X2Y2_1155 = 0x18;
+    uint256 constant FOUNDATION = 0x19;
+    uint256 constant SWEEP_ERC1155 = 0x1a;
 }
