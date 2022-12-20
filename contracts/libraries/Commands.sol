6992321f13b948d43354343375bdffa98dc18ebd
Alice Henshaw <henshawalice@gmail.com>
2022-12-20 17:29:44 +0000

    Increase # of commands

--- contracts/libraries/Commands.sol
+++ contracts/libraries/Commands.sol
@@ -6,9 +6,7 @@ pragma solidity ^0.8.17;
 library Commands {
     // Masks to extract certain bits of commands
     bytes1 internal constant FLAG_ALLOW_REVERT = 0x80;
-    bytes1 internal constant COMMAND_TYPE_MASK = 0x1f;
-    bytes1 internal constant NFT_TYPE_MASK = 0x10;
-    bytes1 internal constant SUB_IF_BRANCH_MASK = 0x08;
+    bytes1 internal constant COMMAND_TYPE_MASK = 0x3f;
 
     // Command Types. Maximum supported command at this moment is 0x1F.
 
@@ -20,7 +18,7 @@ library Commands {
     uint256 constant SWEEP = 0x04;
     uint256 constant TRANSFER = 0x05;
     uint256 constant PAY_PORTION = 0x06;
-    uint256 constant COMMAND_PLACEHOLDER_0x07 = 0x07;
+    // COMMAND_PLACEHOLDER = 0x07;
 
     // Command Types where 0x08<=value<=0x0f, executed in the second nested-if block
     uint256 constant V2_SWAP_EXACT_IN = 0x08;
@@ -29,8 +27,8 @@ library Commands {
     uint256 constant WRAP_ETH = 0x0b;
     uint256 constant UNWRAP_WETH = 0x0c;
     uint256 constant PERMIT2_TRANSFER_FROM_BATCH = 0x0d;
-    uint256 constant COMMAND_PLACEHOLDER_0x0e = 0x0e;
-    uint256 constant COMMAND_PLACEHOLDER_0x0f = 0x0f;
+    // COMMAND_PLACEHOLDER = 0x0e;
+    // COMMAND_PLACEHOLDER = 0x0f;
 
     // Command Types where 0x10<=value<0x18, executed in the third nested-if block
     uint256 constant SEAPORT = 0x10;
@@ -49,6 +47,6 @@ library Commands {
     uint256 constant X2Y2_1155 = 0x1b;
     uint256 constant FOUNDATION = 0x1c;
     uint256 constant SWEEP_ERC1155 = 0x1d;
-    uint256 constant COMMAND_PLACEHOLDER_0x1e = 0x1e;
-    uint256 constant COMMAND_PLACEHOLDER_0x1f = 0x1f;
+    // COMMAND_PLACEHOLDER = 0x1e;
+    // COMMAND_PLACEHOLDER = 0x1f;
 }
