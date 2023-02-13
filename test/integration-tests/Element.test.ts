c39cdc5bab35cf8f90a9bc84e8b4d43c56bd7b3d
Eric Zhong <eric.zhong@uniswap.org>
2023-02-13 15:37:47 -0500

    Add API response order, still stuck on invalid signer w/ new order

--- test/integration-tests/Element.test.ts
+++ test/integration-tests/Element.test.ts
@@ -9,14 +9,14 @@ import hre from 'hardhat'
 import { BigNumber, Contract } from 'ethers'
 import { abi as ERC721_ABI } from '../../artifacts/solmate/src/tokens/ERC721.sol/ERC721.json'
 import { expect } from 'chai'
-import { EXAMPLE_NFT_SELL_ORDER, EXAMPLE_NFT_SELL_ORDER_SIG } from './shared/protocolHelpers/element'
+import { getOrder } from './shared/protocolHelpers/element'
 // TODO: Uncomment after getting api response
-// import { element721Orders } from './shared/protocolHelpers/element'
+import { element721Orders } from './shared/protocolHelpers/element'
 
 const { ethers } = hre
 
 const ELEMENT_721_INTERFACE = new ethers.utils.Interface(ELEMENT_ABI)
-const ZED_HORSE_ADDRESS = '0xA5F1Ea7DF861952863dF2e8d1312f7305dabf215'
+const ELEMENT_POLYGON_ADDRESS = "0xEAF5453b329Eb38Be159a872a6ce91c9A8fb0260"
 
 describe.only('Element Market polygon', () => {
   let alice: SignerWithAddress
@@ -26,23 +26,23 @@ describe.only('Element Market polygon', () => {
   let zedHorse: ERC721
   let element: Contract
 
-  const jsonRpcUrl =  `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` // `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+  const {order, signature, value} = getOrder(element721Orders[0])
+  const nftContractAddress = order.nft
 
   beforeEach(async () => {
-    planner = new RoutePlanner()
-    alice = await ethers.getSigner(ALICE_ADDRESS)
     await hre.network.provider.request({
       method: 'hardhat_reset',
       params: [
         {
           forking: {
-            jsonRpcUrl: jsonRpcUrl,
-            blockNumber: 39069302 - 1,
+            jsonRpcUrl: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+            blockNumber: 39257188,
           },
         },
       ],
-    }) 
-
+    })
+    planner = new RoutePlanner()
+    alice = await ethers.getSigner(ALICE_ADDRESS)
     await hre.network.provider.request({
       method: 'hardhat_impersonateAccount',
       params: [ALICE_ADDRESS],
@@ -50,12 +50,11 @@ describe.only('Element Market polygon', () => {
 
     permit2 = (await deployPermit2()).connect(alice) as Permit2
     router = (await deployUniversalRouter(permit2)).connect(alice) as UniversalRouter
-    zedHorse = new ethers.Contract(ZED_HORSE_ADDRESS, ERC721_ABI).connect(alice) as ERC721
-    element = new ethers.Contract('0xEAF5453b329Eb38Be159a872a6ce91c9A8fb0260', ELEMENT_721_INTERFACE).connect(alice) as Contract
+    zedHorse = new ethers.Contract(nftContractAddress, ERC721_ABI).connect(alice) as ERC721
+    element = new ethers.Contract(ELEMENT_POLYGON_ADDRESS, ELEMENT_721_INTERFACE).connect(alice) as Contract
   })
 
   it('purchases open order', async () => {
-
     // get block number
     const bn = await ethers.provider.getBlockNumber()
     console.log(bn)
@@ -64,28 +63,29 @@ describe.only('Element Market polygon', () => {
     ).toNumber();
     console.log(blockNumber)
 
-    const hash = await element.callStatic.getERC721SellOrderHash(EXAMPLE_NFT_SELL_ORDER)
+    console.log(order, signature)
+
+    // should be 0x6ea39eb9ebaa33f9895454298c7df93dd582dd7159ea3616b6f9ab60eb2a0cc0
+    const hash = await element.callStatic.getERC721SellOrderHash(order)
     console.log(hash)
-    const hashNonce = await element.callStatic.getHashNonce(EXAMPLE_NFT_SELL_ORDER.maker)
+    const hashNonce = await element.callStatic.getHashNonce(order.maker)
     console.log(hashNonce)
-    const status = await element.callStatic.getERC721SellOrderStatus(EXAMPLE_NFT_SELL_ORDER)
+    const status = await element.callStatic.getERC721SellOrderStatus(order)
     console.log(status)
     expect(status).to.eq(1, 'order should be open')
 
-    const takerSigner = await ethers.getImpersonatedSigner("0x224d253edb8c3e4b620e30f0f1904f7d1448223d")
-
-    const value = BigNumber.from(EXAMPLE_NFT_SELL_ORDER.erc20TokenAmount) // since in example we use native token
+    const value = BigNumber.from(order.erc20TokenAmount) // since in example we use native token
     const calldata = ELEMENT_721_INTERFACE.encodeFunctionData('buyERC721', [
-      EXAMPLE_NFT_SELL_ORDER,
-      EXAMPLE_NFT_SELL_ORDER_SIG,
+      order,
+      signature,
     //   alice.address, // taker
     //   '0x00', // extraData
     ])
 
     console.log(calldata)
 
-    const txn = await takerSigner.sendTransaction({
-        to: "0xEAF5453b329Eb38Be159a872a6ce91c9A8fb0260",
+    const txn = await alice.sendTransaction({
+        to: ELEMENT_POLYGON_ADDRESS,
         data: calldata,
         value: value,
     })
@@ -97,15 +97,15 @@ describe.only('Element Market polygon', () => {
     planner.addCommand(CommandType.ELEMENT_MARKET, [value.toString(), calldata])
     const { commands, inputs } = planner
 
-    const ownerBefore = await zedHorse.ownerOf(EXAMPLE_NFT_SELL_ORDER.nftId)
+    const ownerBefore = await zedHorse.ownerOf(order.nftId)
     const ethBefore = await remoteProvider.getBalance(alice.address)
 
     const receipt = await (await router['execute(bytes,bytes[],uint256)'](commands, inputs, DEADLINE, { value })).wait()
 
-    const ownerAfter = await zedHorse.ownerOf(EXAMPLE_NFT_SELL_ORDER.nftId)
+    const ownerAfter = await zedHorse.ownerOf(order.nftId)
     const ethAfter = await remoteProvider.getBalance(alice.address)
 
-    expect(ownerBefore).to.eq(EXAMPLE_NFT_SELL_ORDER.maker)
+    expect(ownerBefore).to.eq(order.maker)
     expect(ownerAfter).to.eq(alice.address)
     expect(ethBefore.sub(ethAfter)).to.eq(value.add(receipt.gasUsed.mul(receipt.effectiveGasPrice)))
   })
--- test/integration-tests/shared/protocolHelpers/element.ts
+++ test/integration-tests/shared/protocolHelpers/element.ts
@@ -7,34 +7,9 @@ const { ethers } = hre
 
 export const element721Orders = JSON.parse(
     fs.readFileSync('test/integration-tests/shared/orders/Element.json', { encoding: 'utf8' })
-)
+).data.orders
 
 export const element721Interface = new ethers.utils.Interface(ELEMENT_721_ABI)
-
-/*
-  {
-        "contractAddress": "0x88af41822c65a64e9614d3784fa1c99b8a02e5f5",
-        "tokenId": "2",
-        "standard": "element-ex-v3",
-        "orderHash": "0xa44550c8883cc36fb98266e45bbae2569db44f8b7cd55e91dc39f89833716654",
-        "paymentToken": "0x0000000000000000000000000000000000000000",
-        "maker": "0x633f6c7e25ee757d12643a32ce1586ac9e8542d5",
-        "listingTime": 1665488710,
-        "side": 1,
-        "saleKind": 0,
-        "price": 0.025,
-        "isValid": true,
-        "errorDetail": "string",
-        "taker": "0x0000000000000000000000000000000000000000",
-        "expirationTime": 1666093622,
-        "quantity": "1",
-        "priceBase": 0.2,
-        "priceUsd": 309.7634,
-        "schema": "ERC721",
-        "basePrice": "120000000000000000",
-        "exchangeData": "{\"order\":{\"maker\":\"0x633f6c7e25ee757d12643a32ce1586ac9e8542d5\",\"taker\":\"0x0000000000000000000000000000000000000000\",\"expiry\":\"0x000000000000000000000000000000000000000000000000636122e0636a5d9c\",\"nonce\":\"99\",\"erc20Token\":\"0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\",\"erc20TokenAmount\":\"174000000000000000\",\"fees\":[{\"recipient\":\"0x00cA62445B06a9aDc1879a44485B4eFdcB7b75F3\",\"amount\":\"16000000000000000\",\"feeData\":\"0x\"},{\"recipient\":\"0x9226f7df5e316df051f0490ce3b753c51695d0bb\",\"amount\":\"10000000000000000\",\"feeData\":\"0x\"}],\"nft\":\"0x88af41822c65a64e9614d3784fa1c99b8a02e5f5\",\"nftId\":\"64\",\"hashNonce\":\"0\"},\"signature\":{\"signatureType\":0,\"v\":28,\"r\":\"0xc5f7bf427f39c3ce3fe39b7c981b7d60f2805a970caae6bd971d476431731706\",\"s\":\"0x04313baeb7397394ac40af76969be987e89bb221a862b5f0f97082d503fa6c16\"}}"
-  }
-*/
 
 export type ElementOrderSignature = {
     signatureType: number // 0 for 721 and 1 for presigned
@@ -54,27 +29,26 @@ export interface Property {
     propertyData: string;
 }
 
-// (0xFfd3b35d3aeadD47c0A99259eE8be899983D9441, 0x0000000000000000000000000000000000000000, 1675972593, 1, 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE, 34500000000000000000, [], 0xA5F1Ea7DF861952863dF2e8d1312f7305dabf215, 152807)
-// (0, 28, 0x2102a204f2f62acf6a44c7a43c0f9a3d972231cee0ab69f682301a29d09c0f29, 0x6a6095f94a58856df46c286c792a08ae2f256f3a32f4e502f70a912e73761216)
+// 39257188
 
-export const EXAMPLE_NFT_SELL_ORDER: NFTSellOrder = {
-    maker: "0xFfd3b35d3aeadD47c0A99259eE8be899983D9441",
-    taker: "0x0000000000000000000000000000000000000000",
-    expiry: "1675972593",
-    nonce: "1",
-    erc20Token: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // native token for Element
-    erc20TokenAmount: "34500000000000000000",
-    fees: [],
-    nft: "0xA5F1Ea7DF861952863dF2e8d1312f7305dabf215",
-    nftId: "152807"
-}
+// export const EXAMPLE_NFT_SELL_ORDER: NFTSellOrder = {
+//     maker: "0xFfd3b35d3aeadD47c0A99259eE8be899983D9441",
+//     taker: "0x0000000000000000000000000000000000000000",
+//     expiry: "1675972593",
+//     nonce: "1",
+//     erc20Token: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", // native token for Element
+//     erc20TokenAmount: "34500000000000000000",
+//     fees: [],
+//     nft: "0xA5F1Ea7DF861952863dF2e8d1312f7305dabf215",
+//     nftId: "152807"
+// }
 
-export const EXAMPLE_NFT_SELL_ORDER_SIG: ElementOrderSignature = {
-    signatureType: 0,
-    v: 28,
-    r: "0x2102a204f2f62acf6a44c7a43c0f9a3d972231cee0ab69f682301a29d09c0f29",
-    s: "0x6a6095f94a58856df46c286c792a08ae2f256f3a32f4e502f70a912e73761216"
-}
+// export const EXAMPLE_NFT_SELL_ORDER_SIG: ElementOrderSignature = {
+//     signatureType: 0,
+//     v: 28,
+//     r: "0x2102a204f2f62acf6a44c7a43c0f9a3d972231cee0ab69f682301a29d09c0f29",
+//     s: "0x6a6095f94a58856df46c286c792a08ae2f256f3a32f4e502f70a912e73761216"
+// }
 
 // Signing over this:
 export interface NFTSellOrder {
@@ -102,29 +76,58 @@ export interface ERC1155SellOrder {
     erc1155TokenAmount: string;
 }
 
+const EXAMPLE_API_ORDER = {
+    "basicCollections": [
+        {
+            "nftAddress": "0x56d46be8dd52ccd0551350e9e783a8282584e868",
+            "platformFee": 200,
+            "royaltyFeeRecipient": "0x0000000000000000000000000000000000000000",
+            "royaltyFee": 0,
+            "items": [
+                {
+                    "erc20TokenAmount": "1200000000000000000",
+                    "nftId": "1540"
+                }
+            ]
+        }
+    ],
+    "collections": null,
+    "startNonce": 2,
+    "nonce": 2,
+    "hashNonce": "0",
+    "platformFeeRecipient": "0xd207842d66b715df6ea08cf52f025b9e2ed28788",
+    "v": 28,
+    "r": "0xa837991196760d5e2953ce0ebb386ff85db84e918645850cc0eb9e60c67e9892",
+    "s": "0x2117cea84c0c2bac7cfda6b799ec376fb35d8d1c5492be7a87f8f8fd324d963e",
+    "listingTime": 1676318726,
+    "expirationTime": 1684094785,
+    "maker": "0x3d2d40700cb8ac8114e669ea4a70ff61bfea802a",
+    "hash": "0x2039d65fead7c00ab1704290b4ddfe84c5ab9156f4dc6384ce8e5568b56f20f6",
+    "paymentToken": "0x0000000000000000000000000000000000000000"
+}
 
-
-// export function getOrder(apiOrder: any): { order: Order; signature: ElementOrderSignature; value: BigNumber } {
-//     const exchangeData = JSON.parse(apiOrder.exchangeData)
-//     const order = {
-//         maker: apiOrder.maker,
-//         taker: apiOrder.taker,
-//         expiry: BigNumber.from(apiOrder.expirationTime),
-//         nonce: BigNumber.from(exchangeData.nonce),
-//         erc20Token: apiOrder.paymentToken,
-//         erc20TokenAmount: BigNumber.from(apiOrder.price),
-//         fees: [],
-//         nft: apiOrder.contractAddress,
-//         nftId: BigNumber.from(apiOrder.tokenId),
-//     }
-//     const value = BigNumber.from(apiOrder.priceBase)
-//     const signature = {
-//         signatureType: BigNumber.from(0), // TODO: don't think we have access to this data
-//         v: BigNumber.from(exchangeData.v),
-//         r: exchangeData.r,
-//         s: exchangeData.s,
-//     }
-//     return { order, signature, value }
-// }
+export function getOrder(apiOrder: any): { order: NFTSellOrder; signature: ElementOrderSignature; value: BigNumber } {
+    const exchangeData: typeof EXAMPLE_API_ORDER = JSON.parse(apiOrder.exchangeData)
+    
+    const order = {
+        maker: apiOrder.maker,
+        taker: apiOrder.taker,
+        expiry: apiOrder.expirationTime,
+        nonce: String(exchangeData.nonce),
+        erc20Token: apiOrder.paymentToken,
+        erc20TokenAmount: exchangeData.basicCollections[0].items[0].erc20TokenAmount,
+        fees: [],
+        nft: apiOrder.contractAddress,
+        nftId: apiOrder.tokenId,
+    }
+    const value = BigNumber.from(exchangeData.basicCollections[0].items[0].erc20TokenAmount)
+    const signature = {
+        signatureType: 0, // TODO: don't think we have access to this data
+        v: exchangeData.v,
+        r: exchangeData.r,
+        s: exchangeData.s,
+    }
+    return { order, signature, value }
+}
 
 
