cb47191476cf9d31b4d0a43636d52c02782fb1f0
Eric Zhong <eric.zhong@uniswap.org>
2023-02-13 13:25:55 -0500

    switch to alchemy to get around missing node errors

--- test/integration-tests/Element.test.ts
+++ test/integration-tests/Element.test.ts
@@ -26,6 +26,8 @@ describe.only('Element Market polygon', () => {
   let zedHorse: ERC721
   let element: Contract
 
+  const jsonRpcUrl =  `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}` // `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+
   beforeEach(async () => {
     planner = new RoutePlanner()
     alice = await ethers.getSigner(ALICE_ADDRESS)
@@ -34,7 +36,7 @@ describe.only('Element Market polygon', () => {
       params: [
         {
           forking: {
-            jsonRpcUrl: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            jsonRpcUrl: jsonRpcUrl,
             blockNumber: 39069302 - 1,
           },
         },
@@ -45,25 +47,32 @@ describe.only('Element Market polygon', () => {
       method: 'hardhat_impersonateAccount',
       params: [ALICE_ADDRESS],
     })
-    // permit2 = (await deployPermit2()).connect(alice) as Permit2
-    // router = (await deployUniversalRouter(permit2)).connect(alice) as UniversalRouter
+
+    permit2 = (await deployPermit2()).connect(alice) as Permit2
+    router = (await deployUniversalRouter(permit2)).connect(alice) as UniversalRouter
     zedHorse = new ethers.Contract(ZED_HORSE_ADDRESS, ERC721_ABI).connect(alice) as ERC721
     element = new ethers.Contract('0xEAF5453b329Eb38Be159a872a6ce91c9A8fb0260', ELEMENT_721_INTERFACE).connect(alice) as Contract
   })
 
   it('purchases open order', async () => {
-    
+
+    // get block number
+    const bn = await ethers.provider.getBlockNumber()
+    console.log(bn)
+    const blockNumber = BigNumber.from(
+      await hre.network.provider.send("eth_blockNumber")
+    ).toNumber();
+    console.log(blockNumber)
+
     const hash = await element.callStatic.getERC721SellOrderHash(EXAMPLE_NFT_SELL_ORDER)
     console.log(hash)
+    const hashNonce = await element.callStatic.getHashNonce(EXAMPLE_NFT_SELL_ORDER.maker)
+    console.log(hashNonce)
     const status = await element.callStatic.getERC721SellOrderStatus(EXAMPLE_NFT_SELL_ORDER)
     console.log(status)
     expect(status).to.eq(1, 'order should be open')
 
     const takerSigner = await ethers.getImpersonatedSigner("0x224d253edb8c3e4b620e30f0f1904f7d1448223d")
-
-    // get block number
-    const blockNumber = await ethers.provider.getBlockNumber()
-    console.log(blockNumber)
 
     const value = BigNumber.from(EXAMPLE_NFT_SELL_ORDER.erc20TokenAmount) // since in example we use native token
     const calldata = ELEMENT_721_INTERFACE.encodeFunctionData('buyERC721', [
@@ -89,12 +98,12 @@ describe.only('Element Market polygon', () => {
     const { commands, inputs } = planner
 
     const ownerBefore = await zedHorse.ownerOf(EXAMPLE_NFT_SELL_ORDER.nftId)
-    const ethBefore = await ethers.provider.getBalance(alice.address)
+    const ethBefore = await remoteProvider.getBalance(alice.address)
 
     const receipt = await (await router['execute(bytes,bytes[],uint256)'](commands, inputs, DEADLINE, { value })).wait()
 
     const ownerAfter = await zedHorse.ownerOf(EXAMPLE_NFT_SELL_ORDER.nftId)
-    const ethAfter = await ethers.provider.getBalance(alice.address)
+    const ethAfter = await remoteProvider.getBalance(alice.address)
 
     expect(ownerBefore).to.eq(EXAMPLE_NFT_SELL_ORDER.maker)
     expect(ownerAfter).to.eq(alice.address)
