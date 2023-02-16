c3cd32815d2d9b87f5d26fa2496b74c7b67092ec
Eric Zhong <eric.zhong@uniswap.org>
2023-02-16 15:03:06 -0500

    Refactor sudo swap test

--- test/integration-tests/Sudoswap.test.ts
+++ test/integration-tests/Sudoswap.test.ts
@@ -2,7 +2,7 @@ import { CommandType, RoutePlanner } from './shared/planner'
 import SUDOSWAP_ABI from './shared/abis/Sudoswap.json'
 import { ERC721, UniversalRouter, Permit2, ERC20 } from '../../typechain'
 import { resetFork } from './shared/mainnetForkHelpers'
-import { ALICE_ADDRESS, DEADLINE } from './shared/constants'
+import { DEADLINE } from './shared/constants'
 import deployUniversalRouter, { deployPermit2 } from './shared/deployUniversalRouter'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import hre from 'hardhat'
@@ -20,67 +20,56 @@ const BASED_GHOUL_ADDRESS = '0xeF1a89cbfAbE59397FfdA11Fc5DF293E9bC5Db90'
 const FRAX_ADDRESS = '0x853d955acef822db058eb8505911ed77f175b99e'
 
 describe.only('Sudoswap', () => {
-  let alice: SignerWithAddress
+  let bob: SignerWithAddress
   let router: UniversalRouter
   let permit2: Permit2
   let planner: RoutePlanner
 
   beforeEach(async () => {
+    await resetFork(16643381) // use recent block
     planner = new RoutePlanner()
-    alice = await ethers.getSigner(ALICE_ADDRESS)
+    bob = (await ethers.getSigners())[1]
+    permit2 = (await deployPermit2()).connect(bob) as Permit2
+    router = (await deployUniversalRouter(permit2)).connect(bob) as UniversalRouter
   })
 
-  // In this test we will buy token ids 80, 35, 93 of Sudolets (0xfa9937555dc20a020a161232de4d2b109c62aa9c),
-  // which costs 0.073 ETH (exactly 73337152777777783 wei)
-  describe('Buy 3 sudolets from sudoswap', () => {
+  // In this test we will buy token ids 173, 239, 240 of Sudolets (0xfa9937555dc20a020a161232de4d2b109c62aa9c),
+  // which costs 0.073 ETH (exactly 73337152777777692 wei)
+  describe('Buy with ETH', () => {
     let sudolets: ERC721
 
     beforeEach(async () => {
-      await resetFork(15740629)
-      await hre.network.provider.request({
-        method: 'hardhat_impersonateAccount',
-        params: [ALICE_ADDRESS],
-      })
-      permit2 = (await deployPermit2()).connect(alice) as Permit2
-      router = (await deployUniversalRouter(permit2)).connect(alice) as UniversalRouter
-
-      sudolets = new ethers.Contract(SUDOLETS_ADDRESS, ERC721_ABI).connect(alice) as ERC721
+      sudolets = new ethers.Contract(SUDOLETS_ADDRESS, ERC721_ABI).connect(bob) as ERC721
     })
 
-    it('purchases token ids 80, 35, 93 of Sudolets', async () => {
-      const value = BigNumber.from('73337152777777783')
+    it('purchases token ids 173, 239, 240 of Sudolets', async () => {
+      const value = BigNumber.from('73337152777777692')
       const calldata = SUDOSWAP_INTERFACE.encodeFunctionData('robustSwapETHForSpecificNFTs', [
-        [[['0x339e7004372e04b1d59443f0ddc075efd9d80360', ['80', '35', '93']], '73337152777777783']],
-        ALICE_ADDRESS,
-        ALICE_ADDRESS,
-        1665685098,
+        [[['0x339e7004372e04b1d59443f0ddc075efd9d80360', ['173', '239', '240']], value]],
+        bob.address,
+        bob.address,
+        1765685098,
       ])
       planner.addCommand(CommandType.SUDOSWAP, [value, calldata])
 
       const { commands, inputs } = planner
       await expect(
         router['execute(bytes,bytes[],uint256)'](commands, inputs, DEADLINE, { value })
-      ).to.changeEtherBalance(alice, value.mul(-1))
+      ).to.changeEtherBalance(bob, value.mul(-1))
 
-      // Expect that alice has the NFTs
-      await expect((await sudolets.ownerOf(80)).toLowerCase()).to.eq(ALICE_ADDRESS)
-      await expect((await sudolets.ownerOf(35)).toLowerCase()).to.eq(ALICE_ADDRESS)
-      await expect((await sudolets.ownerOf(93)).toLowerCase()).to.eq(ALICE_ADDRESS)
+      // Expect that bob has the NFTs
+      expect(await sudolets.ownerOf(173)).to.eq(bob.address)
+      expect(await sudolets.ownerOf(173)).to.eq(bob.address)
+      expect(await sudolets.ownerOf(240)).to.eq(bob.address)
     })
   })
 
   // Buy tokens 2402, 2509 of Based Ghoul (0xeF1a89cbfAbE59397FfdA11Fc5DF293E9bC5Db90)
   describe('Buy using ERC20', () => {
-    let bob: SignerWithAddress
     let fraxToken: ERC20
     let basedGhoul: ERC721
 
     beforeEach(async () => {
-      await resetFork(16643381) // use recent block
-      bob = (await ethers.getSigners())[1]
-      permit2 = (await deployPermit2()).connect(bob) as Permit2
-      router = (await deployUniversalRouter(permit2)).connect(bob) as UniversalRouter
-
       basedGhoul = new ethers.Contract(BASED_GHOUL_ADDRESS, ERC721_ABI).connect(bob) as ERC721
       fraxToken = new ethers.Contract(FRAX_ADDRESS, ERC20_ABI).connect(bob) as ERC20
 
@@ -95,14 +84,8 @@ describe.only('Sudoswap', () => {
     it('it buys tokens 2402, 2509 with FRAX ERC20 token', async () => {
       const value = BigNumber.from('226492000000000000000')
       const ghlFraxPairAddress = '0x9c9604405dea60d5AC4433FCf87D76a0bC6bB68B'
-      // robustSwapERC20ForSpecificNFTs
       const calldata = SUDOSWAP_INTERFACE.encodeFunctionData('robustSwapERC20ForSpecificNFTs', [
-        [
-          [
-            [ghlFraxPairAddress, ['2402', '2509']],
-            value, // max cost of the total trade
-          ],
-        ],
+        [[[ghlFraxPairAddress, ['2402', '2509']], value]],
         value,
         bob.address,
         1776575269,
