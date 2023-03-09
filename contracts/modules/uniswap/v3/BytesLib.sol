128956ff5517da478a972c6f0777d4940fb8449b
Alice <34962750+hensha256@users.noreply.github.com>
2023-03-09 14:39:16 +0100

    Update BytesLib.sol

--- contracts/modules/uniswap/v3/BytesLib.sol
+++ contracts/modules/uniswap/v3/BytesLib.sol
@@ -29,45 +29,53 @@ library BytesLib {
         }
     }
 
-    /// @notice Decode the `arg`-th element in `_bytes` as a dynamic array
+    /// @notice Decode the `_arg`-th element in `_bytes` as a dynamic array
     /// @dev The decoding of `length` and `offset` is universal,
     /// whereas the type declaration of `res` instructs the compiler how to read it.
+    /// @param _bytes The input bytes string to slice
+    /// @param _arg The index of the argument to extract
     /// @return length Length of the array
     /// @return offset Pointer to the data part of the array
-    function toLengthOffset(bytes calldata _bytes, uint256 arg)
+    function toLengthOffset(bytes calldata _bytes, uint256 _arg)
         internal
         pure
         returns (uint256 length, uint256 offset)
     {
         assembly {
-            // The offset of the `arg`-th element is `32 * arg`, which stores the offset of the length pointer.
-            let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, mul(0x20, arg))))
+            // The offset of the `_arg`-th element is `32 * arg`, which stores the offset of the length pointer.
+            let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, mul(0x20, _arg))))
             length := calldataload(lengthPtr)
             offset := add(lengthPtr, 0x20)
         }
     }
 
-    /// @notice Decode the `arg`-th element in `_bytes` as `bytes`
-    function toBytes(bytes calldata _bytes, uint256 arg) internal pure returns (bytes calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, arg);
+    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes`
+    /// @param _bytes The input bytes string to extract a bytes string from
+    /// @param _arg The index of the argument to extract
+    function toBytes(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes calldata res) {
+        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
         assembly {
             res.length := length
             res.offset := offset
         }
     }
 
-    /// @notice Decode the `arg`-th element in `_bytes` as `address[]`
-    function toAddressArray(bytes calldata _bytes, uint256 arg) internal pure returns (address[] calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, arg);
+    /// @notice Decode the `_arg`-th element in `_bytes` as `address[]`
+    /// @param _bytes The input bytes string to extract an address array from
+    /// @param _arg The index of the argument to extract
+    function toAddressArray(bytes calldata _bytes, uint256 _arg) internal pure returns (address[] calldata res) {
+        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
         assembly {
             res.length := length
             res.offset := offset
         }
     }
 
-    /// @notice Decode the `arg`-th element in `_bytes` as `bytes[]`
-    function toBytesArray(bytes calldata _bytes, uint256 arg) internal pure returns (bytes[] calldata res) {
-        (uint256 length, uint256 offset) = toLengthOffset(_bytes, arg);
+    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes[]`
+    /// @param _bytes The input bytes string to extract a bytes array from
+    /// @param _arg The index of the argument to extract
+    function toBytesArray(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes[] calldata res) {
+        (uint256 length, uint256 offset) = toLengthOffset(_bytes, _arg);
         assembly {
             res.length := length
             res.offset := offset
