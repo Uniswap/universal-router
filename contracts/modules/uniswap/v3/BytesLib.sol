f5e8861f7499e17da81227839bcf49615930eda4
Alice Henshaw <henshawalice@gmail.com>
2023-03-31 10:56:04 +0100

    Length offset check

--- contracts/modules/uniswap/v3/BytesLib.sol
+++ contracts/modules/uniswap/v3/BytesLib.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 /// @title Library for Bytes Manipulation
-/// Based on Gonçalo Sá's BytesLib - but updated and heavily editted
 pragma solidity ^0.8.0;
 
+import {Constants} from '../../../libraries/Constants.sol';
+
 library BytesLib {
+    error SliceOutOfBounds();
+
     /// @notice Returns the address starting at byte 0
     /// @dev length and overflow checks must be carried out before calling
     /// @param _bytes The input bytes string to slice
     /// @return _address The address starting at byte 0
     function toAddress(bytes calldata _bytes) internal pure returns (address _address) {
+        if (_bytes.length < Constants.ADDR_SIZE) revert SliceOutOfBounds();
         assembly {
             _address := shr(96, calldataload(_bytes.offset))
         }
@@ -22,6 +26,7 @@ library BytesLib {
     /// @return fee The uint24 starting at byte 20
     /// @return token1 The address at byte 23
     function toPool(bytes calldata _bytes) internal pure returns (address token0, uint24 fee, address token1) {
+        if (_bytes.length < Constants.V3_POP_OFFSET) revert SliceOutOfBounds();
         assembly {
             let firstWord := calldataload(_bytes.offset)
             token0 := shr(96, firstWord)
@@ -42,12 +47,15 @@ library BytesLib {
         pure
         returns (uint256 length, uint256 offset)
     {
+        uint256 relativeOffset;
         assembly {
             // The offset of the `_arg`-th element is `32 * arg`, which stores the offset of the length pointer.
             let lengthPtr := add(_bytes.offset, calldataload(add(_bytes.offset, shl(5, _arg))))
             length := calldataload(lengthPtr)
             offset := add(lengthPtr, 0x20)
+            relativeOffset := sub(offset, _bytes.offset)
         }
+        if (_bytes.length < length + relativeOffset) revert SliceOutOfBounds();
     }
 
     /// @notice Decode the `_arg`-th element in `_bytes` as `bytes`
