43127c25a5ca00e9a69908b824dfd5265f98452f
Alice Henshaw <henshawalice@gmail.com>
2023-04-19 13:31:30 +0100

    cprrecting comment

--- contracts/libraries/Commands.sol
+++ contracts/libraries/Commands.sol
@@ -70,5 +70,5 @@ library Commands {
     uint256 constant SEAPORT_V1_4 = 0x20;
     uint256 constant EXECUTE_SUB_PLAN = 0x21;
     uint256 constant APPROVE_ERC20 = 0x22;
-    // COMMAND_PLACEHOLDER for 0x24 to 0x3f (all unused)
+    // COMMAND_PLACEHOLDER for 0x23 to 0x3f (all unused)
 }
