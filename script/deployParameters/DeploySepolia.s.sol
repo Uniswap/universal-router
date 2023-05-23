e11132c8f1eabe5bdfc31f631881959a6ff8c06a
Alice <34962750+hensha256@users.noreply.github.com>
2023-05-23 14:37:45 +0100

    Deploy sepolia support (#298)

---
+++ script/deployParameters/DeploySepolia.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeploySepolia is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14,
+            seaportV1_5: 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC,
+            seaportV1_4: 0x00000000000001ad428e4906aE43D8F9852d0dD6,
+            openseaConduit: 0x1E0049783F008A0085193E00003D00cd54003c71,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x0227628f3F023bb0B980b67D528571c95c6DaC1c,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0x5302086A3a25d473aAbBd0356eFf8Dd811a4d89B;
+    }
+}
