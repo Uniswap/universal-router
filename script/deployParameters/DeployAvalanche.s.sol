2b3f04b13fdfedbcf26975be42dc6cf5f884a895
Alice <34962750+hensha256@users.noreply.github.com>
2023-06-20 20:01:14 +0100

    Deploy URv1.2 to avalanche (#306)

---
+++ script/deployParameters/DeployAvalanche.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployAvalanche is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7,
+            seaportV1_5: 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC,
+            seaportV1_4: 0x00000000000001ad428e4906aE43D8F9852d0dD6,
+            openseaConduit: 0x1E0049783F008A0085193E00003D00cd54003c71,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0x5302086A3a25d473aAbBd0356eFf8Dd811a4d89B;
+    }
+}
