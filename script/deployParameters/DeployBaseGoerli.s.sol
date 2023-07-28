96f7708810ed7d4386bbca55173254f37db82f92
Jordan Frankfurt <jordan@uniswap.org>
2023-07-28 11:04:34 -0500

    deploy to base goerli (#308)
    * feat(BASE): send it
    
    * pr feedback
    
    * rename
    
    * rename
    
    * add baseGoerli to hardhat config
    
    ---------
    
    Co-authored-by: Emily Williams <emag3m@gmail.com>

---
+++ script/deployParameters/DeployBaseGoerli.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
+
+contract DeployBaseGoerli is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0x44D627f900da8AdaC7561bD73aA745F132450798,
+            seaportV1_5: 0x00000000000000ADc04C56Bf30aC9d3c0aAF14dC,
+            seaportV1_4: 0x00000000000001ad428e4906aE43D8F9852d0dD6,
+            openseaConduit: 0x1E0049783F008A0085193E00003D00cd54003c71,
+            nftxZap: UNSUPPORTED_PROTOCOL,
+            x2y2: UNSUPPORTED_PROTOCOL,
+            foundation: UNSUPPORTED_PROTOCOL,
+            sudoswap: UNSUPPORTED_PROTOCOL,
+            elementMarket: UNSUPPORTED_PROTOCOL,
+            nft20Zap: UNSUPPORTED_PROTOCOL,
+            cryptopunks: UNSUPPORTED_PROTOCOL,
+            looksRareV2: UNSUPPORTED_PROTOCOL,
+            routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
+            looksRareToken: UNSUPPORTED_PROTOCOL,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x9323c1d6D800ed51Bd7C6B216cfBec678B7d0BC2,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
+        });
+
+        unsupported = 0x7B46ee9BaB49bd5b37117494689A035b0F187B59;
+    }
+}
