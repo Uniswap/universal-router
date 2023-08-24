e64cafce14b50dccc8db90a477c86c69a94d1e28
Mark Toda <toda.mark@gmail.com>
2023-08-24 12:00:10 -0700

    feat: add new v2 deployments
    V2 was deployed on more chains, this commit adds them to deploy
    parameters for those chains

--- script/deployParameters/DeployArbitrum.s.sol
+++ script/deployParameters/DeployArbitrum.s.sol
@@ -23,9 +23,9 @@ contract DeployArbitrum is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: UNSUPPORTED_PROTOCOL,
+            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
             v3Factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
-            pairInitCodeHash: BYTES32_ZERO,
+            pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
 
--- script/deployParameters/DeployBase.s.sol
+++ script/deployParameters/DeployBase.s.sol
@@ -23,9 +23,9 @@ contract DeployBase is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: UNSUPPORTED_PROTOCOL,
+            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
             v3Factory: 0x33128a8fC17869897dcE68Ed026d694621f6FDfD,
-            pairInitCodeHash: BYTES32_ZERO,
+            pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
 
