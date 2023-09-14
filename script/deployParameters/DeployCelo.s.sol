90449bae072fb1cc267eda64aeae095130bd0216
Mark Toda <toda.mark@gmail.com>
2023-09-14 14:06:17 -0700

    feat: update celo

--- script/deployParameters/DeployCelo.s.sol
+++ script/deployParameters/DeployCelo.s.sol
@@ -23,7 +23,7 @@ contract DeployCelo is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+            v2Factory: 0x79a530c8e2fA8748B7B40dd3629C0520c2cCf03f,
             v3Factory: 0xAfE208a311B21f13EF87E33A90049fC17A7acDEc,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
