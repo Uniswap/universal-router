6681d53545377291145fa1e4b52e73cace5bd5d2
hensha256 <henshawalice@gmail.com>
2023-10-26 11:45:57 -0400

    functions

--- contracts/CalldataOptRouter.sol
+++ contracts/CalldataOptRouter.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.8.17;
 import {V2SwapRouter} from './modules/uniswap/v2/V2SwapRouter.sol';
 import {V3SwapRouter} from './modules/uniswap/v3/V3SwapRouter.sol';
 
-contract CalldataOptRouter is V2SwapRouter, V3SwapRouter {
+abstract contract CalldataOptRouter is V2SwapRouter, V3SwapRouter {
 
+    function v2SwapExactTokenForToken();
+    function v2SwapTokenForExactToken();
+    function v2SwapExactETHForToken();
+    function v2SwapTokenForExactETH();
+    function v3SwapExactTokenForToken();
+    function v3SwapTokenForExactToken();
+    function v3SwapExactETHForToken();
+    function v3SwapTokenForExactETH();
+    
 }
