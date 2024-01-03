1ff8a10988b6fb6e04b4f46439cafb809ee8bc72
Austin Adams <austino256@gmail.com>
2024-01-03 16:37:16 -0500

    fix: updated v2 factories
    sepolia does not need upgrading

--- script/deployParameters/DeployBSC.s.sol
+++ script/deployParameters/DeployBSC.s.sol
@@ -23,7 +23,7 @@ contract DeployBSC is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+            v2Factory: 0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6,
             v3Factory: 0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
--- script/deployParameters/DeployOptimism.s.sol
+++ script/deployParameters/DeployOptimism.s.sol
@@ -23,7 +23,7 @@ contract DeployOptimism is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+            v2Factory: 0x0c3c1c532F1e39EdF36BE9Fe0bE1410313E074Bf,
             v3Factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
