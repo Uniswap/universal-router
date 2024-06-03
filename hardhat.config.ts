630ce53f326e924b53c7d0d6f89f9ca70e6c00c6
Kyryl Riabov <kyryl.ryabov@gmail.com>
2024-06-03 16:32:36 +0300

    Updated networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ import "@nomicfoundation/hardhat-foundry";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-// const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : undefined
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : undefined
 
 import { HardhatUserConfig } from 'hardhat/config'
 
@@ -55,39 +55,11 @@ const config: HardhatUserConfig = {
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts
     },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    arbitrumRinkeby: {
-      url: `https://rinkeby.arbitrum.io/rpc`,
-    },
-    arbitrum: {
-      url: `https://arb1.arbitrum.io/rpc`,
-    },
-    optimismKovan: {
-      url: `https://kovan.optimism.io`,
-    },
-    optimism: {
-      url: `https://mainnet.optimism.io`,
-    },
-    polygon: {
-      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-    },
-    base: {
-      url: `https://developer-access-mainnet.base.org`,
-    },
-    baseGoerli: {
-      url: `https://goerli.base.org`,
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts
     },
   },
   namedAccounts: {
