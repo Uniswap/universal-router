bfa0535f33ae8a27ab8d67467ff4d582996f7ed9
saucepoint <saucepoint@protonmail.com>
2024-10-17 17:02:55 -0700

    updating some deploys for sepolia L2s

--- script/deployParameters/DeployOPSepolia.s.sol
+++ script/deployParameters/DeployOPSepolia.s.sol
@@ -9,15 +9,15 @@ contract DeployBaseSepolia is DeployUniversalRouter {
         params = RouterParameters({
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
             weth9: 0x4200000000000000000000000000000000000006,
-            v2Factory: 0x7Ae58f10f7849cA6F5fB71b7f45CB416c9204b1e,
+            v2Factory: UNSUPPORTED_PROTOCOL,
             v3Factory: 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54,
-            v4PoolManager: 0x7Da1D65F8B249183667cdE74C5CBD46dD38AA829,
+            v4PoolManager: 0xE5dF461803a59292c6c03978c17857479c40bc46,
             v3NFTPositionManager: 0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2,
-            v4PositionManager: 0xcDbe7b1ed817eF0005ECe6a3e576fbAE2EA5EAFE
+            v4PositionManager: 0xEf3853450006cE9FB12B540486c920c9a705F502
         });
 
-        unsupported = 0x76870DEbef0BE25589A5CddCe9B1D99276C73B4e;
+        unsupported = 0xFC885F37F5A9FA8159c8dBb907fc1b0C2fB31323;
     }
 }
--- script/deployParameters/DeployUnichainSepolia.s.sol
+++ script/deployParameters/DeployUnichainSepolia.s.sol
@@ -9,13 +9,13 @@ contract DeployBaseSepolia is DeployUniversalRouter {
         params = RouterParameters({
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
             weth9: 0x4200000000000000000000000000000000000006,
-            v2Factory: 0x7Ae58f10f7849cA6F5fB71b7f45CB416c9204b1e,
-            v3Factory: 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24,
+            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+            v3Factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54,
-            v4PoolManager: 0x7Da1D65F8B249183667cdE74C5CBD46dD38AA829,
-            v3NFTPositionManager: 0x27F971cb582BF9E50F397e4d29a5C7A34f11faA2,
-            v4PositionManager: 0xcDbe7b1ed817eF0005ECe6a3e576fbAE2EA5EAFE
+            v4PoolManager: 0xC81462Fec8B23319F288047f8A03A57682a35C1A,
+            v3NFTPositionManager: 0xB7F724d6dDDFd008eFf5cc2834edDE5F9eF0d075,
+            v4PositionManager: 0xB433cB9BcDF4CfCC5cAB7D34f90d1a7deEfD27b9
         });
 
         unsupported = 0x76870DEbef0BE25589A5CddCe9B1D99276C73B4e;
