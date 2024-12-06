5726a983739e0db689b0b758e4a35ccf81a5ca0a
dianakocsis <diana.kocsis@uniswap.org>
2024-12-06 13:53:12 -0500

    worldcoin addresses

--- script/deployParameters/DeployWorldchain.s.sol
+++ script/deployParameters/DeployWorldchain.s.sol
@@ -7,15 +7,15 @@ import {RouterParameters} from 'contracts/types/RouterParameters.sol';
 contract DeployWorldchain is DeployUniversalRouter {
     function setUp() public override {
         params = RouterParameters({
-            permit2: 0x0000000000000000000000000000000000000000,
-            weth9: 0x0000000000000000000000000000000000000000,
-            v2Factory: 0x0000000000000000000000000000000000000000,
-            v3Factory: 0x0000000000000000000000000000000000000000,
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0x4200000000000000000000000000000000000006,
+            v2Factory: 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f,
+            v3Factory: 0x7a5028BDa40e7B173C278C5342087826455ea25a,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
-            poolInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
-            v4PoolManager: 0x0000000000000000000000000000000000000000,
-            v3NFTPositionManager: 0x0000000000000000000000000000000000000000,
-            v4PositionManager: 0x0000000000000000000000000000000000000000
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54,
+            v4PoolManager: address(0),
+            v3NFTPositionManager: 0xec12a9F9a09f50550686363766Cc153D03c27b5e,
+            v4PositionManager: address(0)
         });
     }
 }
