9ba1c222e7cbe9dad213bf14ae957a62dd2e7d55
saucepoint <saucepoint@protonmail.com>
2024-12-11 16:26:24 -0500

    new addresses

---
+++ script/deployParameters/DeployArbitrumSepolia.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
+import {RouterParameters} from 'contracts/types/RouterParameters.sol';
+
+contract DeployArbitrumSepolia is DeployUniversalRouter {
+    function setUp() public override {
+        params = RouterParameters({
+            permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
+            weth9: 0x980B62Da83eFf3D4576C647993b0c1D7faf17c73,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54,
+            v4PoolManager: 0xfb3e0c6f74eb1a21cc1da29aec80d2dfe6c9a317,
+            v3NFTPositionManager: 0x6b2937Bde17889EDCf8fbD8dE31C3C2a70Bc4d65,
+            v4PositionManager: 0xac631556d3d4019c95769033b5e719dd77124bac
+        });
+
+        unsupported = UNSUPPORTED_PROTOCOL;
+    }
+}
--- script/deployParameters/DeployUnichainSepolia.s.sol
+++ script/deployParameters/DeployUnichainSepolia.s.sol
@@ -13,9 +13,9 @@ contract DeployUnichainSepolia is DeployUniversalRouter {
             v3Factory: 0x1F98431c8aD98523631AE4a59f267346ea31F984,
             pairInitCodeHash: 0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54,
-            v4PoolManager: 0xC81462Fec8B23319F288047f8A03A57682a35C1A,
+            v4PoolManager: 0x00b036b58a818b1bc34d502d3fe730db729e62ac,
             v3NFTPositionManager: 0xB7F724d6dDDFd008eFf5cc2834edDE5F9eF0d075,
-            v4PositionManager: 0xB433cB9BcDF4CfCC5cAB7D34f90d1a7deEfD27b9
+            v4PositionManager: 0xf969aee60879c54baaed9f3ed26147db216fd664
         });
 
         unsupported = 0x76870DEbef0BE25589A5CddCe9B1D99276C73B4e;
