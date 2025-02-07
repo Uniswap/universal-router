ed65751101e9d28276a24e69cb8d2d5d2234af55
dianakocsis <diana.kocsis@uniswap.org>
2025-02-07 14:05:15 -0500

    some fixes

--- script/deployParameters/DeploySoneium.s.sol
+++ script/deployParameters/DeploySoneium.s.sol
@@ -9,13 +9,15 @@ contract DeploySoneium is DeployUniversalRouter {
         params = RouterParameters({
             permit2: 0x000000000022D473030F116dDEE9F6B43aC78BA3,
             weth9: 0x4200000000000000000000000000000000000006,
-            v2Factory: 0x8702463e73f74d0b6765aBceb314Ef07aCb92650,
-            v3Factory: 0x8702463e73f74d0b6765aBceb314Ef07aCb92650,
-            pairInitCodeHash: 0x0000000000000000000000000000000000000000000000000000000000000000,
-            poolInitCodeHash: 0x0000000000000000000000000000000000000000000000000000000000000000,
+            v2Factory: UNSUPPORTED_PROTOCOL,
+            v3Factory: UNSUPPORTED_PROTOCOL,
+            pairInitCodeHash: BYTES32_ZERO,
+            poolInitCodeHash: BYTES32_ZERO,
             v4PoolManager: 0x360E68faCcca8cA495c1B759Fd9EEe466db9FB32,
             v3NFTPositionManager: 0x8702463e73f74d0b6765aBceb314Ef07aCb92650,
             v4PositionManager: 0x1b35d13a2E2528f192637F14B05f0Dc0e7dEB566
         });
+
+        unsupported = 0x8702463e73f74d0b6765aBceb314Ef07aCb92650;
     }
 }
