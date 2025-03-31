20c847b94eeaf52c5fd12653a31ee80cac24a0dc
Leo Vigna <leo.vigna@gmail.com>
2025-03-31 17:32:07 -0300

    add CALL_TARGET command

---
+++ contracts/libraries/CalldataCallTargetDecoder.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title Library for abi decoding in calldata
+library CalldataCallTargetDecoder {
+    using CalldataCallTargetDecoder for bytes;
+
+    error SliceOutOfBounds();
+
+    /// @notice mask used for offsets and lengths to ensure no overflow
+    /// @dev no sane abi encoding will pass in an offset or length greater than type(uint32).max
+    ///      (note that this does deviate from standard solidity behavior and offsets/lengths will
+    ///      be interpreted as mod type(uint32).max which will only impact malicious/buggy callers)
+    uint256 constant OFFSET_OR_LENGTH_MASK = 0xffffffff;
+    uint256 constant OFFSET_OR_LENGTH_MASK_AND_WORD_ALIGN = 0xffffffe0;
+
+    /// @notice equivalent to SliceOutOfBounds.selector, stored in least-significant bits
+    uint256 constant SLICE_ERROR_SELECTOR = 0x3b99b53d;
+
+    /// @dev equivalent to: abi.decode(params, (address, uint256, bytes)) in calldata
+    function decodeCallTarget(
+        bytes calldata params
+    ) internal pure returns (address target, uint256 value, bytes calldata data) {
+        // no length check performed, as there is a length check in `toBytes`
+        assembly ("memory-safe") {
+            target := calldataload(params.offset)
+            value := calldataload(add(params.offset, 0x20))
+        }
+
+        data = params.toBytes(2);
+    }
+
+    /// @notice Decode the `_arg`-th element in `_bytes` as `bytes`
+    /// @param _bytes The input bytes string to extract a bytes string from
+    /// @param _arg The index of the argument to extract
+    function toBytes(bytes calldata _bytes, uint256 _arg) internal pure returns (bytes calldata res) {
+        uint256 length;
+        assembly ("memory-safe") {
+            // The offset of the `_arg`-th element is `32 * arg`, which stores the offset of the length pointer.
+            // shl(5, x) is equivalent to mul(32, x)
+            let lengthPtr := add(
+                _bytes.offset,
+                and(calldataload(add(_bytes.offset, shl(5, _arg))), OFFSET_OR_LENGTH_MASK)
+            )
+            // the number of bytes in the bytes string
+            length := and(calldataload(lengthPtr), OFFSET_OR_LENGTH_MASK)
+            // the offset where the bytes string begins
+            let offset := add(lengthPtr, 0x20)
+            // assign the return parameters
+            res.length := length
+            res.offset := offset
+
+            // if the provided bytes string isnt as long as the encoding says, revert
+            if lt(add(_bytes.length, _bytes.offset), add(length, offset)) {
+                mstore(0, SLICE_ERROR_SELECTOR)
+                revert(0x1c, 4)
+            }
+        }
+    }
+}
